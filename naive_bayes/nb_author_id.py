e3b8555a3a7c5486f20a1eca76dd9b6c021f5e57
Dana <Dana@maxs-iMac.local>
2015-05-11 09:37:18 -0500

    Finish first assignment naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -4,6 +4,8 @@
     this is the code to accompany the Lesson 1 (Naive Bayes) mini-project 
 
     use a Naive Bayes Classifier to identify emails by their authors
+
+    The NB algorithm will use prior probability to classify emails to their authors.
     
     authors and labels:
     Sara has label 0
@@ -22,13 +24,30 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+def nb(features_train, features_test, labels_train, labels_test):
+	from sklearn.naive_bayes import GaussianNB
+	#first create the classifier
+	classifier = GaussianNB()
+
+	#train algorithm and measure how long it takes
+	t0 = time()
+	classifier.fit(features_train, labels_train)
+	print "training time:", round(time()-t0, 3), "s"
+
+	#test algorithm and measure how long it takes
+	t1 = time()
+	predictions = classifier.predict(features_test)
+	print "testing time:", round(time()-t1, 3), "s"
+
+	#evaluate accuracy of predictions
+	from sklearn.metrics import accuracy_score
+	#compares my predicted labels to the testing labels
+	print "the accuracy of the classifier is: " + str(accuracy_score(predictions, labels_test)) 
+		#"which means that it was able to correctly classify that percentage of emails to the author"
 
 
-
-#########################################################
-### your code goes here ###
+nb(features_train, features_test, labels_train, labels_test)
 
 
-#########################################################
 
 
